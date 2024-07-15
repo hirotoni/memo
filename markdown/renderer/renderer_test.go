404eece41c0ea6e23084abb890948a70406823b0
hirotoni <yuki.pdg@gmail.com>
2024-07-15 13:04:55 +0900

    refact: add test for renderEmphasis

--- markdown/renderer/renderer_test.go
+++ markdown/renderer/renderer_test.go
@@ -2,6 +2,7 @@ package renderer
 
 import (
 	"bufio"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -73,6 +74,56 @@ func TestMarkdownRenderer_renderHeading(t *testing.T) {
 
 			assert.NoError(w.Flush())
 			assert.Equal(tt.wantStr, buf.String())
+		})
+	}
+}
+
+func TestMarkdownRenderer_renderEmphasis(t *testing.T) {
+	type args struct {
+		node     ast.Node
+		entering bool
+	}
+	type wants struct {
+		status ast.WalkStatus
+		str    string
+		err    bool
+	}
+
+	tests := []struct {
+		name  string
+		args  args
+		wants wants
+	}{
+		{
+			name:  "entering true",
+			args:  args{node: &ast.Emphasis{Level: 1}, entering: true},
+			wants: wants{status: ast.WalkContinue, str: "*", err: false},
+		},
+		{
+			name:  "entering false",
+			args:  args{node: &ast.Emphasis{Level: 1}, entering: false},
+			wants: wants{status: ast.WalkContinue, str: "*", err: false},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewMarkdownRenderer()
+			sb := new(strings.Builder)
+			w := bufio.NewWriter(sb)
+			dummySource := []byte("dummy source")
+
+			got, err := r.renderEmphasis(w, dummySource, tt.args.node, tt.args.entering)
+			if (err != nil) != tt.wants.err {
+				t.Errorf("MarkdownRenderer.renderEmphasis() error = %v, wantErr %v", err, tt.wants.err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wants.status) {
+				t.Errorf("MarkdownRenderer.renderEmphasis() = %v, want %v", got, tt.wants.status)
+			}
+
+			assert.NoError(t, w.Flush())
+			assert.Equal(t, tt.wants.str, sb.String())
 		})
 	}
 }
