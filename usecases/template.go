95967b789f4645a060bb88b97d3c0b5cf4211212
hirotoni <yuki.pdg@gmail.com>
2024-12-31 17:05:53 +0900

    refact: replace Heading struct initialization with NewHeading and NewTemplate constructors

--- usecases/template.go
+++ usecases/template.go
@@ -9,46 +9,44 @@ import (
 
 var (
 	// daily memo
-	HEADING_NAME_TITLE     = models.Heading{Level: 1, Text: "daily memo"}
-	HEADING_NAME_TODAYSTIP = models.Heading{Level: 2, Text: "today's tip"}
-	HEADING_NAME_TODOS     = models.Heading{Level: 2, Text: "todos"}
-	HEADING_NAME_WANTTODOS = models.Heading{Level: 2, Text: "wanttodos"}
-	HEADING_NAME_MEMOS     = models.Heading{Level: 2, Text: "memos"}
-
+	HEADING_NAME_TITLE     = models.NewHeading(1, "daily memo")
+	HEADING_NAME_TODAYSTIP = models.NewHeading(2, "today's tip")
+	HEADING_NAME_TODOS     = models.NewHeading(2, "todos")
+	HEADING_NAME_WANTTODOS = models.NewHeading(2, "wanttodos")
+	HEADING_NAME_MEMOS     = models.NewHeading(2, "memos")
 	// weekly report
-	HEADING_NAME_WEEKLYREPORT = models.Heading{Level: 1, Text: "Weekly Report"}
-
+	HEADING_NAME_WEEKLYREPORT = models.NewHeading(1, "Weekly Report")
 	// tips index
-	HEADING_NAME_TIPSINDEX = models.Heading{Level: 1, Text: "Tips Index"}
+	HEADING_NAME_TIPSINDEX = models.NewHeading(1, "Tips Index")
 )
 
 var (
-	TemplateDailymemo = models.Template{
-		Headings: []models.Heading{
-			HEADING_NAME_TITLE,
-			HEADING_NAME_TODAYSTIP,
-			HEADING_NAME_TODOS,
-			HEADING_NAME_WANTTODOS,
-			HEADING_NAME_MEMOS,
-		},
+	// daily memo
+	dailymemoHeadings = []models.Heading{
+		HEADING_NAME_TITLE,
+		HEADING_NAME_TODAYSTIP,
+		HEADING_NAME_TODOS,
+		HEADING_NAME_WANTTODOS,
+		HEADING_NAME_MEMOS,
 	}
-	TemplateWeeklyReport = models.Template{
-		Headings: []models.Heading{
-			HEADING_NAME_WEEKLYREPORT,
-		},
+	weeklyReportHeadings = []models.Heading{
+		HEADING_NAME_WEEKLYREPORT,
 	}
-	TemplateTips = models.Template{
-		Headings: []models.Heading{
-			{Level: 1, Text: "sushi (<- tip category)"},
-			{Level: 2, Text: "how to eat sushi (<- tip title in heading level 2)"},
-			{Level: 2, Text: "how to roll sushi (<- another tip)"},
-		},
+	tipsHeadings = []models.Heading{
+		models.NewHeading(1, "sushi (<- tip category)"),
+		models.NewHeading(2, "how to eat sushi (<- tip title in heading level 2)"),
+		models.NewHeading(2, "how to roll sushi (<- another tip)"),
 	}
-	TemplateTipsIndex = models.Template{
-		Headings: []models.Heading{
-			HEADING_NAME_TIPSINDEX,
-		},
+	tipsIndexHeadings = []models.Heading{
+		HEADING_NAME_TIPSINDEX,
 	}
+)
+
+var (
+	TemplateDailymemo    = models.NewTemplate(dailymemoHeadings)
+	TemplateWeeklyReport = models.NewTemplate(weeklyReportHeadings)
+	TemplateTips         = models.NewTemplate(tipsHeadings)
+	TemplateTipsIndex    = models.NewTemplate(tipsIndexHeadings)
 )
 
 func GenerateTemplateString(t models.Template) string {
