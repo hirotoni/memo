4742dd41c979e48939a09eecad17566926982951
hirotoni <yuki.pdg@gmail.com>
2025-01-13 21:01:35 +0900

    feat: refactor Dailymemo handling by moving NewDailymemoFromFilepath logic to DailymemoRepo.Entry method

--- repos/dailymemo.go
+++ repos/dailymemo.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/hirotoni/memo/config"
@@ -23,6 +24,28 @@ func NewDailymemoRepo(config *config.TomlConfig) *DailymemoRepo {
 
 var FILENAME_REGEX = `\d{4}-\d{2}-\d{2}-\S{3}\.md`
 
+func (repo *DailymemoRepo) Entry(fpath string) models.Dailymemo {
+	basename := filepath.Base(fpath)
+	datestring, found := strings.CutSuffix(basename, ".md")
+	if !found {
+		log.Fatal("failed to cut suffix.")
+	}
+	date, err := time.Parse(FULL_LAYOUT, datestring)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := os.ReadFile(fpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return models.Dailymemo{
+		Filepath: fpath,
+		BaseName: basename,
+		Date:     date,
+		Content:  b,
+	}
+}
+
 func (repo *DailymemoRepo) Entries() []models.Dailymemo {
 	entries, err := os.ReadDir(repo.config.DailymemoDir()) // sorted by filename(=date)
 	if err != nil {
@@ -38,8 +61,8 @@ func (repo *DailymemoRepo) Entries() []models.Dailymemo {
 	}
 
 	dms := make([]models.Dailymemo, 0, len(wantfiles))
-	for _, file := range wantfiles {
-		dm := models.NewDailymemoFromFilepath(file)
+	for _, fpath := range wantfiles {
+		dm := repo.Entry(fpath)
 		dms = append(dms, dm)
 	}
 
@@ -54,5 +77,5 @@ func (repo *DailymemoRepo) FindByDate(date string) models.Dailymemo {
 		log.Fatal(err)
 	}
 	filepath := filepath.Join(repo.config.DailymemoDir(), date+".md")
-	return models.NewDailymemoFromFilepath(filepath)
+	return repo.Entry(filepath)
 }
--- repos/dailymemo_test.go
+++ repos/dailymemo_test.go
@@ -8,6 +8,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDailymemoRepo_Entry(t *testing.T) {
+	assert := assert.New(t)
+
+	config := config.NewTomlConfig("testdata", 7, markdown.NewGoldmarkWrapper())
+	repo := NewDailymemoRepo(config)
+	tests := []struct {
+		name string
+	}{
+		{name: "Test Entry"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := repo.Entry("testdata/dailymemo/2024-12-30-Mon.md")
+			assert.Equal("testdata/dailymemo/2024-12-30-Mon.md", e.Filepath)
+			assert.Equal("2024-12-30-Mon.md", e.BaseName)
+			assert.Equal("2024-12-30", e.Date.Format("2006-01-02"))
+			assert.Equal("testdata/dailymemo/2024-12-30-Mon.md", e.Filepath)
+		})
+	}
+
+}
+
 func TestDailymemoRepo_Entries(t *testing.T) {
 	assert := assert.New(t)
 
